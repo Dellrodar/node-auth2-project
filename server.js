9ba0aafecfae7ded19770e71ca630612a436b86e
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-09-06 20:57:18 -0400

    Endpoints * All MVP endpoints created and tested * Added users router to /api route

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const usersRouter = require('./users/users-router')
 
-const server = express()
-const port = process.env.PORT || 5000
+const server = express();
+const port = process.env.PORT || 5000;
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-server.use(cookieParser())
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(cookieParser());
+server.use("/api", usersRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
-
+	console.log(err);
 	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+		message: 'Something went wrong',
+	});
+});
 
 server.listen(port, () => {
-	console.log(`Running at http://localhost:${port}`)
-})
+	console.log(`Running at http://localhost:${port}`);
+});
---
+++ users/users-router.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('./users-model');
+
+const router = express.Router();
+
+router.get("/users", async (req, res, next) => {
+  try {
+		return res.json(await Users.find());
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/users/:id", async (req, res, next) => {
+  try {
+		return res.json(await Users.findById(req.params.id));
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const {username, password, department } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+			return res.status(409).json({
+				message: 'Username is already taken',
+			});
+		}
+
+		const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 15),
+      department,
+    });
+
+    return res.status(201).json(newUser);
+
+  } catch(err) {
+    return next(err);
+  }
+});
+
+router.post("/login", async ( req, res, next) => {
+  try {
+		const { username, password } = req.body;
+		const user = await Users.findBy({ username }).first();
+
+		if (!user) {
+			return res.status(401).json({
+				message: 'Invalid Credentials',
+			});
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: 'Invalid Credentials',
+			});
+    }
+
+    const token = jwt.sign({
+      userID: user.id,
+      userRole: user.department,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+    });
+	} catch (err) {
+		next(err);
+	}
+});
+
+
+module.exports = router;
